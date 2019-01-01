fb4f9cc38dc7e0c3c82f7c261f728b8e581e9144
Daniel G <19241189+DanGSun@users.noreply.github.com>
2019-01-01 05:19:10 +0300

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Super-Duper installer for everything u need!
 
-apk add python3
-apk add git
+sudo apk add python3
+sudo apk add git
 echo "First step is finished. Installing services."
 #TODO
